fdbe8eed19a2cf5f6a90fec15e75dda6a0b66990
Tomas Vahancik <vhn.448@gmail.com>
2020-02-16 10:17:48 +0100

    Fix movie detail responsiveness

--- src/pages/detail/MovieDetail.js
+++ src/pages/detail/MovieDetail.js
@@ -2,9 +2,9 @@ import React, { useEffect } from 'react';
 import { loadMovie } from '../../actions/movie_detail_actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import FavoriteMark from './FavoriteMark';
 import Error from '../../components/Error';
 import Loading from '../../components/Loading';
+import FavoriteMark from './FavoriteMark';
 
 const MovieDetail = () => {
   const {movieId} = useParams();
@@ -12,8 +12,8 @@ const MovieDetail = () => {
   useEffect(() => {
     dispatch(loadMovie(movieId));
   }, []);
-  let movie = useSelector(state => state.movieDetail.movie);
-  let error = useSelector(state => state.movieDetail.error);
+  const movie = useSelector(state => state.movieDetail.movie);
+  const error = useSelector(state => state.movieDetail.error);
   if (error) {
     return <Error error={error}/>;
   }
@@ -21,9 +21,11 @@ const MovieDetail = () => {
     <div className="container mt-2">
       <Loading loading={movie === undefined}/>
       {movie &&
-      <div className="media">
-        <img className="mr-3 shadow" src={movie.Poster} alt={movie.Title + ' poster'}/>
-        <div className="media-body">
+      <div className="row">
+        <div className="col-md-3 text-md-right col-lg-4 text-lg-left">
+          <img className="shadow img-fluid" src={movie.Poster} alt={movie.Title + ' poster'}/>
+        </div>
+        <div className="col-md-9 text-md-left col-lg-8 text-lg-left">
           <div className="row justify-content-start">
             <div className="col">
               <h3>{movie.Title} <FavoriteMark/></h3>
@@ -33,7 +35,7 @@ const MovieDetail = () => {
             <div className="col-2">
               <strong>Year: </strong>
             </div>
-            <div className="col-3">
+            <div className="col-4">
               {movie.Year}
             </div>
             <div className="col-2">
@@ -47,7 +49,7 @@ const MovieDetail = () => {
             <div className="col-2">
               <strong>Director: </strong>
             </div>
-            <div className="col-3">
+            <div className="col-4">
               {movie.Director}
             </div>
             <div className="col-2">
@@ -61,7 +63,7 @@ const MovieDetail = () => {
             <div className="col-2">
               <strong>Genre: </strong>
             </div>
-            <div className="col-3">
+            <div className="col-4">
               {movie.Genre}
             </div>
             <div className="col-2">
@@ -112,7 +114,8 @@ const MovieDetail = () => {
             </div>
           </div>
         </div>
-      </div>}
+      </div>
+      }
     </div>
   );
 };
